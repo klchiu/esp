e9e094401a6bfc44b32eb62bfd1fd37e4ecbc27d
Kuan-Lin Chiu <chiu@cs.columbia.edu>
2022-04-19 18:19:51 -0400

    [max_fft]: change printf to fprintf in fft2.cpp

--- accelerators/stratus_hls/fft2_stratus/hw/src/fft2.cpp
+++ accelerators/stratus_hls/fft2_stratus/hw/src/fft2.cpp
@@ -72,7 +72,7 @@ void fft2::load_input()
             // data word is wider than NoC links
             dma_info_t dma_info(offset * DMA_BEAT_PER_WORD, len * DMA_BEAT_PER_WORD, DMA_SIZE);
 #else
-            printf("LOAD DMA INFO : rem %u : off = %u , len %u\n", rem, (offset / DMA_WORD_PER_BEAT),
+            fprintf(stderr, "LOAD DMA INFO : rem %u : off = %u , len %u\n", rem, (offset / DMA_WORD_PER_BEAT),
                    (len / DMA_WORD_PER_BEAT));
             dma_info_t dma_info(offset / DMA_WORD_PER_BEAT, len / DMA_WORD_PER_BEAT, DMA_SIZE);
 #endif
@@ -109,14 +109,14 @@ void fft2::load_input()
                 }
             }
 #endif
-            printf("LOAD hit the load-compute handshake...\n");
+            fprintf(stderr, "LOAD hit the load-compute handshake...\n");
             this->load_compute_handshake();
             this->load_to_store_handshake();
             loads_done++;
         } // for (rem = length downto 0 )
     }     // Load scope
 
-    // printf("LOAD process is done -- did %u loads\n", loads_done);
+    // fprintf(stderr, "LOAD process is done -- did %u loads\n", loads_done);
     // Conclude
     {
         this->process_done();
@@ -184,7 +184,7 @@ void fft2::store_output()
 #endif
         // Chunking
         for (int rem = length; rem > 0; rem -= PLM_OUT_WORD) {
-            printf("STORE hit the store-compute handshake...\n");
+            fprintf(stderr, "STORE hit the store-compute handshake...\n");
             this->store_compute_handshake();
 
             // Configure DMA transaction
@@ -193,7 +193,7 @@ void fft2::store_output()
             // data word is wider than NoC links
             dma_info_t dma_info(offset * DMA_BEAT_PER_WORD, len * DMA_BEAT_PER_WORD, DMA_SIZE);
 #else
-            printf("STORE DMA INFO : rem %u : off = %u , len = %u\n", rem, (offset / DMA_WORD_PER_BEAT),
+            fprintf(stderr, "STORE DMA INFO : rem %u : off = %u , len = %u\n", rem, (offset / DMA_WORD_PER_BEAT),
                    (len / DMA_WORD_PER_BEAT));
             dma_info_t dma_info(offset / DMA_WORD_PER_BEAT, len / DMA_WORD_PER_BEAT, DMA_SIZE);
 #endif
@@ -237,7 +237,7 @@ void fft2::store_output()
             this->store_to_load_handshake();
         } // for (rem = length downto 0
     }     // Store scope
-    // printf("STORE process is done - did %u stores!\n", stores_done);
+    // fprintf(stderr, "STORE process is done - did %u stores!\n", stores_done);
     // Conclude
     {
         this->accelerator_done();
@@ -286,7 +286,7 @@ void fft2::compute_kernel()
         do_shift    = config.do_shift;
         // scale_factor = config.scale_factor;
     }
-    // printf("COMPUTE: logn_samples %u num_samples %u num_ffts %u inverse %u shift %u\n", logn_samples, num_samples,
+    // fprintf(stderr, "COMPUTE: logn_samples %u num_samples %u num_ffts %u inverse %u shift %u\n", logn_samples, num_samples,
     // num_ffts, do_inverse, do_shift);
 
     // Compute
@@ -297,7 +297,7 @@ void fft2::compute_kernel()
         uint32_t out_rem     = out_length;
         unsigned max_in_ffts = 1 << (MAX_LOGN_SAMPLES - logn_samples);
         unsigned ffts_done   = 0;
-        printf("COMPUTE: in_len %u : max_in_ffts %u >> %u = %u\n", in_length, MAX_LOGN_SAMPLES, logn_samples,
+        fprintf(stderr, "COMPUTE: in_len %u : max_in_ffts %u >> %u = %u\n", in_length, MAX_LOGN_SAMPLES, logn_samples,
                max_in_ffts);
         // Chunking : Load/Store Memory transfers (refill memory)
         for (int in_rem = in_length; in_rem > 0; in_rem -= PLM_IN_WORD) {
@@ -305,20 +305,20 @@ void fft2::compute_kernel()
             uint32_t out_len = out_rem > PLM_OUT_WORD ? PLM_OUT_WORD : out_rem;
 
             // Compute FFT(s) in the PLM
-            printf("COMPUTE INFO : in_rem %u : in_len %u :: out_rem %u : out_len %u\n", in_rem, in_len, out_rem,
+            fprintf(stderr, "COMPUTE INFO : in_rem %u : in_len %u :: out_rem %u : out_len %u\n", in_rem, in_len, out_rem,
                    out_len);
-            printf("COMPUTE hit the compute-load handshake...\n");
+            fprintf(stderr, "COMPUTE hit the compute-load handshake...\n");
             this->compute_load_handshake();
             unsigned rem_ffts = (num_ffts - ffts_done);
             unsigned in_ffts  = (rem_ffts > max_in_ffts) ? max_in_ffts : rem_ffts;
-            printf("COMPUTE has %u rem_ffts : proceeding to the next %u FFT computations...\n", rem_ffts, in_ffts);
+            fprintf(stderr, "COMPUTE has %u rem_ffts : proceeding to the next %u FFT computations...\n", rem_ffts, in_ffts);
             for (unsigned fftn = 0; fftn < in_ffts; fftn++) {
                 unsigned offset = fftn * num_samples; // Offset into Mem for start of this FFT
-                printf("COMPUTE: starting FFT %u of %u = %u : offset = %u\n", fftn, in_ffts, ffts_done, offset);
+                fprintf(stderr, "COMPUTE: starting FFT %u of %u = %u : offset = %u\n", fftn, in_ffts, ffts_done, offset);
                 int sin_sign = (do_inverse) ? -1 : 1; // This modifes the mySin
                                                       // values used below
                 if (do_inverse && do_shift) {
-                    // printf("ACCEL: Calling Inverse-Do-Shift\n");
+                    // fprintf(stderr, "ACCEL: Calling Inverse-Do-Shift\n");
                     fft2_do_shift(offset, num_samples, logn_samples);
                 }
 
@@ -330,16 +330,16 @@ void fft2::compute_kernel()
 
 FFT2_SINGLE_L1:
                 for (unsigned s = 1; s <= logn_samples; s++) {
-                    // printf(" L1 : FFT %u = %u : s %u\n", fftn, ffts_done, s);
+                    // fprintf(stderr, " L1 : FFT %u = %u : s %u\n", fftn, ffts_done, s);
                     m = 1 << s;
                     CompNum wm(myCos(s), sin_sign * mySin(s));
-                    // printf("s: %d\n", s);
-                    // printf("wm.re: %.15g, wm.im: %.15g\n", wm.re, wm.im);
+                    // fprintf(stderr, "s: %d\n", s);
+                    // fprintf(stderr, "wm.re: %.15g, wm.im: %.15g\n", wm.re, wm.im);
 
 FFT2_SINGLE_L2:
                     for (unsigned k = 0; k < num_samples; k += m) {
                         // if (k < 2) {
-                        //     printf("  L2 : FFT %u = %u : s %u : k %u\n", fftn, ffts_done, s, k);
+                        //     fprintf(stderr, "  L2 : FFT %u = %u : s %u : k %u\n", fftn, ffts_done, s, k);
                         // }
 
                         CompNum w((FPDATA)1, (FPDATA)0);
@@ -351,7 +351,7 @@ FFT2_SINGLE_L3:
                             int kj  = offset + k + j;
                             int kjm = offset + k + j + md2;
                             // if ((k == 0) && (j == 0)) {
-                            //     printf("  L3 : FFT %u = %u : k %u j %u md2 %u : kj %u kjm %u : kji %u kjmi %u\n",
+                            //     fprintf(stderr, "  L3 : FFT %u = %u : k %u j %u md2 %u : kj %u kjm %u : kji %u kjmi %u\n",
                             //     fftn, ffts_done, k, j, md2, kj, kjm, 2*kj, 2*kjm);
                             // }
                             CompNum akj, akjm;
@@ -384,7 +384,7 @@ FFT2_SINGLE_L3:
                                 // cout << "DFT: A0 " << kj << ": " << A0[kj].re.to_hex() << " " << A0[kj].im.to_hex()
                                 // << endl; cout << "DFT: A0 " << kjm << ": " << A0[kjm].re.to_hex() << " " <<
                                 // A0[kjm].im.to_hex() << endl; if ((k == 0) && (j == 0)) {
-                                //         printf("  L3 : WROTE A0 %u and %u and %u and %u\n", 2*kj, 2*kj + 1, 2*kjm,
+                                //         fprintf(stderr, "  L3 : WROTE A0 %u and %u and %u and %u\n", 2*kj, 2*kj + 1, 2*kjm,
                                 //         2*kjm + 1);
                                 // }
                             }
@@ -393,10 +393,10 @@ FFT2_SINGLE_L3:
                 }         // for (s = 1 .. logn_samples)
 
                 if ((!do_inverse) && (do_shift)) {
-                    // printf("ACCEL: Calling Non-Inverse Do-Shift\n");
+                    // fprintf(stderr, "ACCEL: Calling Non-Inverse Do-Shift\n");
                     fft2_do_shift(offset, num_samples, logn_samples);
                 }
-                // printf("COMPUTE: done with FF %u = %u\n", fftn, ffts_done);
+                // fprintf(stderr, "COMPUTE: done with FF %u = %u\n", fftn, ffts_done);
                 /*cout << "ACCEL-END : FFT " << ffts_done << " : A0[0] = " << A0[0].to_double() << endl;
                   cout << "ACCEL-END : FFT " << ffts_done << " : A0[1] = " << A0[1].to_double() << endl;
                   cout << "ACCEL-END : FFT " << ffts_done << " : A0[2] = " << A0[2].to_double() << endl;
@@ -405,7 +405,7 @@ FFT2_SINGLE_L3:
                 offset += num_samples; // Offset into Mem for start of this FFT
             }                          // for( n = 0 .. mnum_ffts)
             out_rem -= PLM_OUT_WORD;
-            printf("COMPUTE hit the compute-store handshake...\n");
+            fprintf(stderr, "COMPUTE hit the compute-store handshake...\n");
             this->compute_store_handshake();
         } // for (in_rem = in_length downto 0)
 

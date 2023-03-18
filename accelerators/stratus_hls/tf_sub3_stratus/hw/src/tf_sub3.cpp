cdca4dd7f6df401c64c29c20021bc639c4409dfb
Kuan-Lin Chiu <chiu@cs.columbia.edu>
2023-03-18 15:49:49 -0400

    Add tf_sub3 accelerator

---
+++ accelerators/stratus_hls/tf_sub3_stratus/hw/src/tf_sub3.cpp
@@ -0,0 +1,202 @@
+// Copyright (c) 2011-2021 Columbia University, System Level Design Group
+// SPDX-License-Identifier: Apache-2.0
+
+#include "tf_sub3.hpp"
+#include "tf_sub3_directives.hpp"
+
+
+// Functions
+
+#include "tf_sub3_functions.hpp"
+
+// Processes
+
+void tf_sub3::load_input()
+{
+    uint32_t mem_length          = 0;
+    uint32_t mem_src_dst_offset_1 = 0;
+    uint32_t mem_src_dst_offset_2 = 0;
+    uint32_t index, length;
+
+    // Reset
+    {
+        HLS_PROTO("load-reset");
+
+        this->reset_load_input();
+        accel_ready.ack.reset_ack();
+
+        // User-defined reset code
+
+        wait();
+    }
+
+    // Config
+    {
+        HLS_PROTO("load-config");
+
+        cfg.wait_for_config(); // config process
+        conf_info_t config = this->conf_info.read();
+
+        // User-defined config code
+        mem_length          = config.length;
+        mem_src_dst_offset_1 = config.src_dst_offset_1;
+        mem_src_dst_offset_2 = config.src_dst_offset_2;
+    }
+
+    // Load
+
+
+        HLS_PROTO("load-dma");
+
+        this->load_store_handshake();
+
+        // == BURST (read first input) ==
+        index  = mem_src_dst_offset_1;
+        length = mem_length;
+        dma_info_t dma_info_1(index, length, SIZE_WORD);
+        this->dma_read_ctrl.put(dma_info_1);
+
+        for (unsigned i = 0; i < length; i++) {
+            sc_dt::sc_bv<DMA_WIDTH> pixel_bv = this->dma_read_chnl.get();
+            wait();
+
+            HLS_BREAK_DEP(A0_in1);
+
+            FPDATA_WORD pixel_fp;
+            pixel_fp = pixel_bv.range(63, 0).to_int64();
+
+            A0_in1[i] = pixel_fp;
+            wait();
+        }
+
+        // == BURST (read second input) ==
+        index  = mem_src_dst_offset_2;
+        length = mem_length;
+        dma_info_t dma_info_2(index, length, SIZE_WORD);
+        this->dma_read_ctrl.put(dma_info_2);
+
+        for (unsigned i = 0; i < length; i++) {
+            sc_dt::sc_bv<DMA_WIDTH> pixel_bv = this->dma_read_chnl.get();
+            wait();
+
+            HLS_BREAK_DEP(A0_in2);
+
+            FPDATA_WORD pixel_fp;
+            pixel_fp = pixel_bv.to_int64();
+
+            A0_in2[i] = pixel_fp;
+            wait();
+        }
+
+        this->load_compute_handshake();
+    
+
+    // Conclude
+    {
+        HLS_PROTO("load-done");
+        this->process_done();
+    }
+}
+
+void tf_sub3::store_output()
+{
+    uint32_t mem_length          = 0;
+    uint32_t mem_src_dst_offset_0 = 0;
+    uint32_t index, length;
+
+    // Reset
+    {
+        HLS_PROTO("store-reset");
+
+        this->reset_store_output();
+        accel_ready.req.reset_req();
+
+        // User-defined reset code
+        wait();
+    }
+
+    // Config
+    {
+        HLS_PROTO("store-config");
+
+        cfg.wait_for_config(); // config process
+        conf_info_t config = this->conf_info.read();
+
+        // User-defined config code
+        mem_length          = config.length;
+        mem_src_dst_offset_0 = config.src_dst_offset_0;
+    }
+
+        HLS_PROTO("store-dma");
+
+        this->store_load_handshake();
+        this->store_compute_handshake();
+
+        // Configure DMA write
+        index  = mem_src_dst_offset_0;
+        length = mem_length;
+        dma_info_t dma_info(index, length, SIZE_WORD);
+        this->dma_write_ctrl.put(dma_info);
+
+        for (unsigned i = 0; i < length; i++) {
+
+            HLS_BREAK_DEP(B0_out);
+            FPDATA_WORD             pixel_fp;
+            sc_dt::sc_bv<WORD_SIZE> pixel_bv;
+
+            wait();
+            pixel_fp = B0_out[i];
+
+            // pixel_bv = fp2bv<FPDATA, WORD_SIZE>(pixel_fp);
+            pixel_bv.range(63, 0) = pixel_fp;
+
+            wait();
+            this->dma_write_chnl.put(pixel_bv);
+        }
+    
+
+    // Conclude
+    {
+        HLS_PROTO("store-done");
+        this->accelerator_done();
+        this->process_done();
+    }
+}
+
+void tf_sub3::compute_kernel()
+{
+    uint32_t mem_length = 0;
+
+    // Reset
+    {
+        HLS_PROTO("compute-reset");
+
+        this->reset_compute_kernel();
+
+        // User-defined reset code
+        wait();
+    }
+
+    {
+        HLS_PROTO("compute-config");
+
+        cfg.wait_for_config(); // config process
+        conf_info_t config = this->conf_info.read();
+        mem_length = config.length;
+    }
+
+    // Compute
+        // HLS_UNROLL_LOOP(OFF);
+
+        this->compute_load_handshake();
+
+        // Computing phase implementation
+        sub(mem_length);
+        this->compute_store_handshake();
+
+    // Conclude
+    {
+        HLS_PROTO("compute-done");
+        this->process_done();
+    }
+}
---
+++ accelerators/stratus_hls/tf_sub3_stratus/hw/tb/system.cpp
@@ -0,0 +1,239 @@
+// Copyright (c) 2011-2021 Columbia University, System Level Design Group
+// SPDX-License-Identifier: Apache-2.0
+
+#include <iostream>
+#include <vector>
+#include <sstream>
+#include <fstream>
+#include <stdio.h>
+#include "system.hpp"
+
+// [humu]: use fish_132_17.bin as the test input
+#define INPUT_SIZE INPUT_SIZE_132_17
+
+std::ofstream ofs;
+
+
+void system_t::malloc_arrays(int len)
+{
+    output_0 = (float*)malloc(sizeof(float) * len);
+    input_1 = (float*)malloc(sizeof(float) * len);
+    input_2 = (float*)malloc(sizeof(float) * len);
+    gold_0 = (float*)malloc(sizeof(float) * len);
+}
+
+void system_t::free_arrays()
+{
+    free(output_0);
+    free(input_1);
+    free(input_2);
+    free(gold_0);
+}
+
+void system_t::init_arrays(int len)
+{
+    int i;
+
+    for (i = 0 ; i < len; i++){
+        float val_1 = i *100 / 3.0 - 17;
+        float val_2 = i *100 / 9.0 - 17;
+
+        input_1[i] = val_1;
+        input_2[i] = val_2;
+
+if(i < 10){
+        ESP_REPORT_INFO("%d: val_1: %f\tval_2: %f", i, val_1, val_2);
+}
+
+    }
+}
+
+void system_t::run_pv(int len)
+{
+    int i;
+    for (i = 0 ; i < len; i++){
+        gold_0[i] = input_1[i] - input_2[i];
+    }
+}
+
+// Process
+void system_t::config_proc()
+{
+    // Reset
+    {
+        conf_done.write(false);
+        conf_info.write(conf_info_t());
+        wait();
+    }
+
+    ESP_REPORT_INFO("reset done");
+
+
+    length = 1024;
+
+    malloc_arrays(length);
+
+    init_arrays(length);
+
+    run_pv(length);
+
+    // Config
+    {
+        // length = 1024;
+        base_addr_0  = 0;
+        base_addr_1  = length;
+        base_addr_2  = length + length;
+        
+        conf_info_t config(length, base_addr_0, base_addr_1, base_addr_2);
+
+        ESP_REPORT_INFO("[config]: length = %d", config.length);
+        ESP_REPORT_INFO("[config]: base_addr_0 = %d", config.src_dst_offset_0);
+        ESP_REPORT_INFO("[config]: base_addr_1 = %d", config.src_dst_offset_1);
+        ESP_REPORT_INFO("[config]: base_addr_2 = %d", config.src_dst_offset_2);
+        
+        wait();
+        conf_info.write(config);
+        conf_done.write(true);
+    }
+
+    ESP_REPORT_INFO("config done");
+
+    // Load input data
+    load_memory();
+
+    // Compute
+    {
+        // Print information about begin time
+        sc_time begin_time = sc_time_stamp();
+        ESP_REPORT_TIME(begin_time, "BEGIN - tf_sub3");
+
+        // Wait the termination of the accelerator
+        do {
+            wait();
+        } while (!acc_done.read());
+
+        debug_info_t debug_code = debug.read();
+
+        // Print information about end time
+        sc_time end_time = sc_time_stamp();
+        ESP_REPORT_TIME(end_time, "END - tf_sub3");
+
+        esc_log_latency(sc_object::name(), clock_cycle(end_time - begin_time));
+        wait();
+        conf_done.write(false);
+    }
+
+    // Validate
+    {
+        dump_memory();
+
+        validate();
+    }
+
+    // Conclude
+    {
+        // Clean up
+        ESP_REPORT_INFO("free arrays");
+        free_arrays();
+        ESP_REPORT_INFO("Time to stop");
+        sc_stop();
+    }
+}
+
+// Functions
+void system_t::load_memory()
+{
+    //  Memory initialization:
+    ESP_REPORT_INFO("---- load memory ----");
+    uint32_t mem_base_addr = 0;
+
+    // Load first input
+    mem_base_addr = base_addr_1;
+    ESP_REPORT_INFO("[load_memory]: mem_base_addr: %d", mem_base_addr);
+    for (uint32_t index = 0; index < length; index++) {
+        sc_dt::sc_bv<FPDATA_WL> data_bv;
+        FPDATA                  data_fp;
+
+        // convert float to fx
+        data_fp = FPDATA(input_1[index]);
+        // convert fx to bv
+        data_bv.range(63, 0) = fp2bv<FPDATA, FPDATA_WL>(data_fp);
+
+        mem[mem_base_addr + index].range(63, 0) = data_bv;
+    }
+
+    // Load second input
+    mem_base_addr = base_addr_2;
+    ESP_REPORT_INFO("[load_memory]: mem_base_addr: %d", mem_base_addr);
+    for (uint32_t index = 0; index < length; index++) {
+        sc_dt::sc_bv<FPDATA_WL> data_bv;
+        FPDATA                  data_fp;
+
+        // convert float to fx
+        data_fp = FPDATA(input_2[index]);
+        // convert fx to bv
+        data_bv = fp2bv<FPDATA, FPDATA_WL>(data_fp);
+
+        mem[mem_base_addr + index].range(63, 0) = data_bv;
+    }
+
+    ESP_REPORT_INFO("load memory completed");
+}
+
+void system_t::dump_memory()
+{
+    uint32_t mem_base_addr = 0;
+
+    mem_base_addr = base_addr_0;
+    ESP_REPORT_INFO("[dump_memory]: mem_base_addr: %d", mem_base_addr);
+    for (uint32_t index = 0; index < length; index++) {
+        sc_dt::sc_bv<FPDATA_WL> data_bv;
+        FPDATA                  data_fp;
+
+        data_bv = mem[mem_base_addr + index];
+        // convert bv to fp
+        data_fp = bv2fp<FPDATA, FPDATA_WL>(data_bv);
+        // conver fp to float
+        output_0[index] = (float)data_fp;
+        // printf("[dump] : %d\t%f\n", index, sub[i]);
+    }
+
+    ESP_REPORT_INFO("dump memory completed");
+}
+
+int system_t::validate()
+{
+    ESP_REPORT_INFO("---- validate ----");
+
+    int error_count = 0;
+    int total       = 0;
+
+    for (uint32_t i = 0; i < length; i++) {
+        if (abs(gold_0[i] - output_0[i]) > 0.001) {
+            error_count++;
+            if (error_count < 5) {
+                ESP_REPORT_INFO("%d: out: %f\tgold: %f", i, output_0[i], gold_0[i]);
+            }
+        }
+                    if (total < 5) {
+                ESP_REPORT_INFO("%d: out: %f\tgold: %f", i, output_0[i], gold_0[i]);
+            }
+
+        total++;
+    }
+
+
+
+
+    ESP_REPORT_INFO("====================================================");
+    if (error_count > 0) {
+        ESP_REPORT_INFO("Errors: %d out of %d pixel(s) not match.", error_count, total);
+    } else {
+        ESP_REPORT_INFO("Correct!!");
+    }
+    ESP_REPORT_INFO("====================================================");
+
+    ESP_REPORT_INFO("Validate completed");
+
+    return error_count;
+}

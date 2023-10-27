ef9c84e49335ce10233c769bd4862f77c2a621ce
Kuan-Lin Chiu <chiu@cs.columbia.edu>
2023-10-27 15:56:49 -0400

    Add a working systolic_gemm2_vivado from template

---
+++ accelerators/vivado_hls/systolic_gemm2_vivado/sw/linux/driver/systolic_gemm2_vivado.c
@@ -0,0 +1,134 @@
+// Copyright (c) 2011-2023 Columbia University, System Level Design Group
+// SPDX-License-Identifier: Apache-2.0
+#include <linux/of_device.h>
+#include <linux/mm.h>
+
+#include <asm/io.h>
+
+#include <esp_accelerator.h>
+#include <esp.h>
+
+#include "systolic_gemm2_vivado.h"
+
+#define DRV_NAME	"systolic_gemm2_vivado"
+
+/* <<--regs-->> */
+#define SYSTOLIC_GEMM2_MATRIX_C_DIM_REG 0x48
+#define SYSTOLIC_GEMM2_MATRIX_A_DIM_REG 0x44
+#define SYSTOLIC_GEMM2_MATRIX_B_DIM_REG 0x40
+
+struct systolic_gemm2_vivado_device {
+	struct esp_device esp;
+};
+
+static struct esp_driver systolic_gemm2_driver;
+
+static struct of_device_id systolic_gemm2_device_ids[] = {
+	{
+		.name = "SLD_SYSTOLIC_GEMM2_VIVADO",
+	},
+	{
+		.name = "eb_082",
+	},
+	{
+		.compatible = "sld,systolic_gemm2_vivado",
+	},
+	{ },
+};
+
+static int systolic_gemm2_devs;
+
+static inline struct systolic_gemm2_vivado_device *to_systolic_gemm2(struct esp_device *esp)
+{
+	return container_of(esp, struct systolic_gemm2_vivado_device, esp);
+}
+
+static void systolic_gemm2_prep_xfer(struct esp_device *esp, void *arg)
+{
+	struct systolic_gemm2_vivado_access *a = arg;
+
+	/* <<--regs-config-->> */
+	iowrite32be(a->matrix_C_dim, esp->iomem + SYSTOLIC_GEMM2_MATRIX_C_DIM_REG);
+	iowrite32be(a->matrix_A_dim, esp->iomem + SYSTOLIC_GEMM2_MATRIX_A_DIM_REG);
+	iowrite32be(a->matrix_B_dim, esp->iomem + SYSTOLIC_GEMM2_MATRIX_B_DIM_REG);
+	iowrite32be(a->src_offset, esp->iomem + SRC_OFFSET_REG);
+	iowrite32be(a->dst_offset, esp->iomem + DST_OFFSET_REG);
+
+}
+
+static bool systolic_gemm2_xfer_input_ok(struct esp_device *esp, void *arg)
+{
+	/* struct systolic_gemm2_vivado_device *systolic_gemm2 = to_systolic_gemm2(esp); */
+	/* struct systolic_gemm2_vivado_access *a = arg; */
+
+	return true;
+}
+
+static int systolic_gemm2_probe(struct platform_device *pdev)
+{
+	struct systolic_gemm2_vivado_device *systolic_gemm2;
+	struct esp_device *esp;
+	int rc;
+
+	systolic_gemm2 = kzalloc(sizeof(*systolic_gemm2), GFP_KERNEL);
+	if (systolic_gemm2 == NULL)
+		return -ENOMEM;
+	esp = &systolic_gemm2->esp;
+	esp->module = THIS_MODULE;
+	esp->number = systolic_gemm2_devs;
+	esp->driver = &systolic_gemm2_driver;
+	rc = esp_device_register(esp, pdev);
+	if (rc)
+		goto err;
+
+	systolic_gemm2_devs++;
+	return 0;
+ err:
+	kfree(systolic_gemm2);
+	return rc;
+}
+
+static int __exit systolic_gemm2_remove(struct platform_device *pdev)
+{
+	struct esp_device *esp = platform_get_drvdata(pdev);
+	struct systolic_gemm2_vivado_device *systolic_gemm2 = to_systolic_gemm2(esp);
+
+	esp_device_unregister(esp);
+	kfree(systolic_gemm2);
+	return 0;
+}
+
+static struct esp_driver systolic_gemm2_driver = {
+	.plat = {
+		.probe		= systolic_gemm2_probe,
+		.remove		= systolic_gemm2_remove,
+		.driver		= {
+			.name = DRV_NAME,
+			.owner = THIS_MODULE,
+			.of_match_table = systolic_gemm2_device_ids,
+		},
+	},
+	.xfer_input_ok	= systolic_gemm2_xfer_input_ok,
+	.prep_xfer	= systolic_gemm2_prep_xfer,
+	.ioctl_cm	= SYSTOLIC_GEMM2_VIVADO_IOC_ACCESS,
+	.arg_size	= sizeof(struct systolic_gemm2_vivado_access),
+};
+
+static int __init systolic_gemm2_init(void)
+{
+	return esp_driver_register(&systolic_gemm2_driver);
+}
+
+static void __exit systolic_gemm2_exit(void)
+{
+	esp_driver_unregister(&systolic_gemm2_driver);
+}
+
+module_init(systolic_gemm2_init)
+module_exit(systolic_gemm2_exit)
+
+MODULE_DEVICE_TABLE(of, systolic_gemm2_device_ids);
+
+MODULE_AUTHOR("Emilio G. Cota <cota@braap.org>");
+MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("systolic_gemm2_vivado driver");
